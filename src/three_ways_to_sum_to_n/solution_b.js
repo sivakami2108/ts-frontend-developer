d24164955043111727241fccddbb9aa55bc5f704
sivakami2108 <55121270+sivakami2108@users.noreply.github.com>
2019-09-10 08:58:21 +0530

    Here i ,inheret input data and using instance to call function to calculate the cumulative of Nth number

--- src/three_ways_to_sum_to_n/solution_b.js
+++ src/three_ways_to_sum_to_n/solution_b.js
@@ -3,6 +3,56 @@
 // Input: `n` - any integer from `0` to `Number.MAX_SAFE_INTEGER`.
 // Output: `return` - summation to `n`, i.e. sum_to_n(5) === 1 + 2 + 3 + 4 + 5 === 15.
 
-var sum_to_n = function(n) {
-    // your code here
-};
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp2
+{
+   public class Detail
+    {
+        
+        public
+            int n;
+          public  void get_detail()
+        {
+           
+            Console.WriteLine("Enter the Nth number");
+            n = int.Parse(Console.ReadLine());
+        }
+
+    }
+     class operation : Detail
+        {
+            int sum=0,i=0;
+            public void set_detail()
+            {
+                while (i<= n)
+                {
+                    sum = sum + i;
+                    i++;
+                    }
+                Console.WriteLine("Sum of " + n + "\t number is:" + sum);
+            Console.ReadKey();
+
+
+
+                
+                }
+        }
+    class Program
+    {
+            static void Main(string[] args)
+            {
+                operation obj = new operation();
+                obj.get_detail();
+                obj.set_detail();
+            }
+
+                   
+    }
+}
