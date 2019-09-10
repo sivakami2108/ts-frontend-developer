4eadae35c57ab7e326f92af60d4f02a744118b66
sivakami2108 <55121270+sivakami2108@users.noreply.github.com>
2019-09-10 08:59:57 +0530

    Update solution_c.js

--- src/three_ways_to_sum_to_n/solution_c.js
+++ src/three_ways_to_sum_to_n/solution_c.js
@@ -3,6 +3,39 @@
 // Input: `n` - any integer from `0` to `Number.MAX_SAFE_INTEGER`.
 // Output: `return` - summation to `n`, i.e. sum_to_n(5) === 1 + 2 + 3 + 4 + 5 === 15.
 
-var sum_to_n = function(n) {
-    // your code here
-};
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class operation
+    {
+
+        static void Main()
+        {
+            int number, sum = 0;
+
+            Console.Write("Enter a Number : ");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            if (number < 0)
+            {
+                Console.Write("Please Enter Positive Number");
+            }
+            else
+            {
+                while (number > 0)
+                {
+                    sum += number;
+                    number -= 1;
+                }
+            }
+            Console.WriteLine("The sum is " + sum);
+
+            Console.ReadKey();
+        }
+    }
+}
