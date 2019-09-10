15770c72782decb6ce31f2c5bbc8fb241891ce09
sivakami2108 <55121270+sivakami2108@users.noreply.github.com>
2019-09-10 08:47:47 +0530

    here 'for loop' used to find the cumulative of Nth number

--- src/three_ways_to_sum_to_n/solution_a.js
+++ src/three_ways_to_sum_to_n/solution_a.js
@@ -2,7 +2,36 @@
 // Task: Provide 3 unique implementations of the following function.
 // Input: `n` - any integer from `0` to `Number.MAX_SAFE_INTEGER`.
 // Output: `return` - summation to `n`, i.e. sum_to_n(5) === 1 + 2 + 3 + 4 + 5 === 15.
+Using System;
 
-var sum_to_n = function(n) {
-    // your code here
-};
+namespace program
+{
+    class solution_a
+    {
+      
+            int i, sum = 0,n;
+            public void sum_to_n()
+         {
+            Console.Write("Enter the Nth Number : ");
+            n = int.Parse(Console.ReadLine());
+            for (i = 0; i <= n; i++)
+            {
+                sum = sum + i;
+            }
+            Console.WriteLine("\nSum of N Numbers : " + sum);
+            
+ 
+        }
+    
+
+ class Program
+    {
+        static void Main(string[] args)
+        {
+          solution_a obj=new solution_a();
+          obj.sum_to_n();
+          Console.ReadKey();
+        } 
+    }
+}  
+
